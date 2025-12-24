aea9d74e3fd83dffc8e9d847239c1c18ccc00e61
Narasimha Reddy <49887403+narasimhareddyprostack@users.noreply.github.com>
2025-12-24 10:35:33 +0530

    tst

---
+++ Part-1/class-10-IterativeSt/five.py
@@ -0,0 +1,8 @@
+i=1
+number=2
+limit=10
+while i<limit:
+    if number%2==0:
+        print(number)
+        i+=1
+    number+=1
---
+++ Part-1/class-10-IterativeSt/four.py
@@ -0,0 +1,6 @@
+number=int(input("Enter n of even number:"))
+
+i=2
+while i<=number*2:
+    print(i,end=',')
+    i=i+2
